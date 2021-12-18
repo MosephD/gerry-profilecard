c2894603de11c414bd2f1769df4f27c02382046b
Moseph <joseph@gleam.mx>
2021-12-17 21:50:28 -0800

    StoryBook installed + Profile Card component created

---
+++ stories/ProfileCard.stories.js
@@ -0,0 +1,8 @@
+import "./profilecard.css";
+
+export default {
+  title: "Example/ProfileCard",
+};
+
+export const ProfileCard = () =>
+  '<div class="profile_card"> <div class="profile_card-content"> <div class="profile_image"> <img src="https://avatars.githubusercontent.com/u/49964450?v=4" alt="gerry-picture"> </div> <div class="profile_card-user"> <h1>Gerardo Aguilar</h1> <h2>26</h2> </div> <a href="https://www.google.com/maps/place/Culiac%C3%A1n+Rosales,+Sin./@24.6762629,-107.1048255,9.25z/data=!4m5!3m4!1s0x86bcd6c77747f98f:0x8dd9ebf0eb2d23cc!8m2!3d24.8090649!4d-107.3940117" target="_blank" rel="noopener noreferrer"> <h3 class="location">Culiacán</h3> </a> <div class="profile_card-stats"> <div> <a href="https://github.com/gerrystuff" target="_blank" rel="noopener noreferrer"> <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="github-logo"></a> </div> <div> <a href="https://mx.linkedin.com/in/luis-gerardo-aguilar-padilla-a8ab37173" target="_blank" rel="noopener noreferrer"> <img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" alt="linkedin-logo"></a> </div> <div> <a href="mailto:luis@gleam.mx"> <img src="https://cdn-icons-png.flaticon.com/512/646/646094.png" target="_blank" rel="noopener noreferrer" alt="email-icon"></a> </div> </div> </div> </div>';
